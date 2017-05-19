8bfa15a06311e7e0b2d191a2b96131367749efa0
adrisanchez95 <adri.nervion95@gmail.com>
2017-05-19 19:26:40 +0200

    Add files via upload

---
+++ dudas/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', ready, false);
+
+function crear () {
+	alert ("Añadir un nuevo estudiante");
+}
+
+function borrar () {
+	alert ("¿Estas seguro de que quieres borrar el estudiante?");
+}
